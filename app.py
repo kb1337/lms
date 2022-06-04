05b85cd215db6a9fa870da4a8741ee434c357628
Kadir Burak Gündüz <73403802+kb1337@users.noreply.github.com>
2022-06-04 09:10:24 +0300

    Remove Book releated endpoints from app

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ import sys
 import logging
 from functools import wraps
 from bson import ObjectId
-from flask import Flask, render_template, request, flash, url_for, redirect, session
+from flask import Flask, render_template, redirect, session
 import pymongo
-from datetime import datetime
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -40,6 +39,8 @@ from user import routes
 
 
 def login_required(f):
+    """login_required decorator"""
+
     @wraps(f)
     def wrap(*args, **kwargs):
         if "logged_in" in session:
@@ -51,6 +52,8 @@ def login_required(f):
 
 
 def admin_required(f):
+    """admin_required decorator"""
+
     @wraps(f)
     def wrap(*args, **kwargs):
         if session["user"] == "admin":
@@ -63,12 +66,14 @@ def admin_required(f):
 
 @app.route("/")
 def home():
+    """home"""
     return render_template("home.html")
 
 
 @app.route("/dashboard/")
 @login_required
 def dashboard():
+    """dashboard"""
 
     borrow_history = list(db.borrow_history.find({"user_id": session["user"]["_id"]}))
 
@@ -79,246 +84,6 @@ def dashboard():
     logger.debug(borrow_history[::-1])
 
     return render_template("dashboard.html", borrow_history=borrow_history[::-1])
-
-
-@login_required
-def check_book(book_id: str) -> bool:
-    """Check if book exists in database"""
-
-    # Check if book_id is valid ObjectId
-    if not ObjectId.is_valid(book_id):
-        logger.warning("Invalid book_id: %s", book_id)
-        return False
-
-    # Check if book exists
-    book = list(db.books.find({"_id": ObjectId(book_id)}))
-    if len(book) == 0:
-        logger.warning("Book not found: %s", book_id)
-        return False
-
-    logger.info("%s book found with id='%s'", len(book), book_id)
-    logger.debug("Book: %s", book)
-    return True
-
-
-@app.route("/books")
-@login_required
-def list_books():
-    """books page"""
-    books = list(db.books.find())
-    logger.debug("%s books found", len(books))
-    return render_template("books.html", books=books)
-
-
-@app.route("/book/<book_id>")
-@login_required
-def book_details(book_id):
-    """book details"""
-
-    book = check_book(book_id)
-    if not book:
-        flash("Book not found", "danger")
-        return redirect(url_for("list_books"))
-
-    book = db.books.find_one({"_id": ObjectId(book_id)})
-
-    logger.info("Book: ", book)
-    return render_template("book_details.html", book=book)
-
-
-@app.route("/book/update/<book_id>", methods=["GET", "POST"])
-@login_required
-def update_book(book_id):
-    """book update"""
-
-    # Get book informations from db
-    if request.method == "GET":
-        book = check_book(book_id)
-        if not book:
-            flash("Book not found", "danger")
-            return redirect(url_for("list_books"))
-
-        book = list(db.books.find({"_id": ObjectId(book_id)}))[0]
-        logger.debug("Book: %s", book)
-        return render_template("update_book.html", book=book)
-
-    # Update book with new values
-    elif request.method == "POST":
-        title = request.form.get("title")
-        authors = request.form.get("authors")
-        editors = request.form.get("editors")
-        isbn = request.form.get("isbn")
-        year = request.form.get("year")
-        edition = request.form.get("edition")
-        publisher = request.form.get("publisher")
-        page = request.form.get("page")
-        language = request.form.get("language")
-        image = request.form.get("image")
-        quantity = request.form.get("quantity")
-        price = request.form.get("price")
-
-        logger.debug("Book: %s", request.form)
-
-        authors = str(authors).split(",")
-        authors = [author.strip() for author in authors]
-        editors = str(editors).split(",")
-        editors = [editor.strip() for editor in editors]
-
-        db.books.update_one(
-            {"_id": ObjectId(book_id)},
-            {
-                "$set": {
-                    "title": title,
-                    "authors": authors,
-                    "editors": editors,
-                    "isbn": isbn,
-                    "year": year,
-                    "edition": edition,
-                    "publisher": publisher,
-                    "page": page,
-                    "language": language,
-                    # "image": image,
-                    "quantity": int(quantity),
-                    "price": price,
-                }
-            },
-        )
-
-        logger.info("Updating book with id='%s'", book_id)
-        flash("Book updated successfully", "success")
-        return redirect(url_for("list_books"))
-
-    else:
-        logger.warning("Invalid request method %s", request.method)
-        flash("Invalid request method", "danger")
-        return redirect(url_for("list_books"))
-
-
-@app.route("/book/delete/<book_id>")
-@login_required
-def delete_book(book_id):
-    """book delete"""
-
-    book = check_book(book_id)
-
-    if not book:
-        flash("Book not found", "danger")
-        return redirect(url_for("list_books"))
-
-    db.books.delete_one({"_id": ObjectId(book_id)})
-
-    logger.info("Deleting book with id='%s'", book_id)
-    flash("Book deleted successfully", "success")
-    return redirect(url_for("list_books"))
-
-
-@app.route("/search", methods=["GET"])
-@login_required
-def search_books():
-    """Search book"""
-    search = request.args.get("search")
-
-    if not search or len(search) < 1:
-        return redirect(url_for("list_books"))
-
-    books = list(
-        db.books.find(
-            {
-                "$or": [
-                    {"authors": {"$in": [search]}},
-                    {"editors": {"$in": [search]}},
-                    {"edition": {"$regex": search, "$options": "i"}},
-                    {"title": {"$regex": search, "$options": "i"}},
-                    {"language": {"$regex": search, "$options": "i"}},
-                    {"isbn": {"$regex": search, "$options": "i"}},
-                    {"publisher": {"$regex": search, "$options": "i"}},
-                    {"page": search},
-                    {"year": search},
-                ]
-            }
-        )
-    )
-
-    print(f"\n\n{books}\n\n")
-
-    logger.debug("%s books found", len(books))
-    if not books:
-        flash("Book not found", "danger")
-        return redirect(url_for("list_books"))
-
-    flash(f"{len(books)} books found", "success")
-    return render_template("books.html", books=books)
-
-
-@app.route("/book/borrow/<book_id>")
-@login_required
-def borrow_book(book_id):
-    """borrow book"""
-
-    book = check_book(book_id)
-
-    if not book:
-        flash("Book not found", "danger")
-        return redirect(url_for("list_books"))
-
-    # Check if book is available
-    book = list(db.books.find({"_id": ObjectId(book_id)}))[0]
-    if book["quantity"] <= 0:
-        flash("Book is not available", "danger")
-        return redirect(url_for("list_books"))
-
-    # Update book quantity
-    db.books.update_one({"_id": ObjectId(book_id)}, {"$inc": {"quantity": -1}})
-
-    # Add borrow history
-    db.borrow_history.insert_one(
-        {
-            "user_id": session["user"]["_id"],
-            "book_id": ObjectId(book_id),
-            "borrow_date": datetime.now(),
-        }
-    )
-
-    logger.info("Borrowing book with id='%s'", book_id)
-    flash("Book borrowed successfully", "success")
-    return redirect(url_for("dashboard"))
-
-
-@app.route("/book/return/<record_id>")
-@login_required
-def return_book(record_id):
-    """return book"""
-
-    # Check if record_id is valid ObjectId
-    if not ObjectId.is_valid(record_id):
-        flash("Invalid record id", "danger")
-        return redirect(url_for("dashboard"))
-
-    record = db.borrow_history.find_one({"_id": ObjectId(record_id)})
-    logger.debug(record)
-
-    if not record:
-        flash("Book not found", "danger")
-        return redirect(url_for("list_books"))
-
-    book_id = record["book_id"]
-
-    # Check if book returned before
-    if "return_date" in record.keys():
-        flash("Book is already returned", "danger")
-        return redirect(url_for("dashboard"))
-
-    # Update book quantity
-    db.books.update_one({"_id": ObjectId(book_id)}, {"$inc": {"quantity": 1}})
-
-    db.borrow_history.update_one(
-        {"_id": ObjectId(record_id)},
-        {"$set": {"return_date": datetime.now()}},
-    )
-    logger.info("return_date updated from borrow history. Record ID: '%s'", book_id)
-
-    flash("Book returned successfully", "success")
-    return redirect(url_for("dashboard"))
 
 
 if __name__ == "__main__":
