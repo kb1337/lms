c02db8324aa9dfaa30206d637ad3a4d99881da61
Kadir Burak Gündüz <73403802+kb1337@users.noreply.github.com>
2022-06-06 23:39:30 +0300

    Update route endings with `/`

--- user/routes.py
+++ user/routes.py
@@ -4,19 +4,19 @@ from app import app
 from user.models import User
 
 
-@app.route("/user/signup", methods=["POST"])
+@app.route("/user/signup/", methods=["POST"])
 def signup():
     """signup"""
     return User().signup()
 
 
-@app.route("/user/signout")
+@app.route("/user/signout/")
 def signout():
     """signout"""
     return User().signout()
 
 
-@app.route("/user/login", methods=["POST"])
+@app.route("/user/login/", methods=["POST"])
 def login():
     """login"""
     return User().login()
