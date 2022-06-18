3275b9944ccf34163af59f90e7131e9cd3440314
Kadir Burak Gündüz <73403802+kb1337@users.noreply.github.com>
2022-06-18 12:01:16 +0300

    Fix login_required decorators

--- book/routes.py
+++ book/routes.py
@@ -2,58 +2,63 @@
 
 from app import app
 from book.models import Book
+from user.models import User
 
 
 @app.route("/books/")
+@User.login_required
 def list_books():
     """books page"""
     return Book().list_books()
 
 
 @app.route("/book/<book_id>/")
-# @login_required
+@User.login_required
 def book_details(book_id):
     """book details"""
     return Book().book_details(book_id)
 
 
 @app.route("/book/add/", methods=["GET", "POST"])
-# @login_required
+@User.login_required
+@User.admin_required
 def add_book():
     """Add new book"""
     return Book().add_book()
 
 
 @app.route("/book/update/<book_id>/", methods=["GET", "POST"])
-# @login_required
+@User.login_required
+@User.admin_required
 def update_book(book_id):
     """book update"""
     return Book().update_book(book_id)
 
 
 @app.route("/book/delete/<book_id>/")
-# @login_required
+@User.login_required
+@User.admin_required
 def delete_book(book_id):
     """book delete"""
     return Book().delete_book(book_id)
 
 
 @app.route("/search/", methods=["GET"])
-# @login_required
+@User.login_required
 def search_books():
     """search book"""
     return Book().search_books()
 
 
 @app.route("/book/borrow/<book_id>/")
-# @login_required
+@User.login_required
 def borrow_book(book_id):
     """borrow book"""
     return Book().borrow_book(book_id)
 
 
 @app.route("/book/return/<record_id>/")
-# @login_required
+@User.login_required
 def return_book(record_id):
     """return book"""
     return Book().return_book(record_id)
